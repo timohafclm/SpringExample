1eaf4f4a0926228e3ac19a038839e331c80b6770
igor <timohafclm@mail.ru>
2018-04-05 03:00:54 +0300

    fix @Autowired jdbc

--- src/main/java/example/Controller.java
+++ src/main/java/example/Controller.java
@@ -1,5 +1,6 @@
 package example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @Autowired
+    JDBCLayer jdbc;
 
     @RequestMapping(value="/last_application", method = RequestMethod.GET)
     public Application lastApplication(@RequestParam(value = "id")String id){
-        JDBCLayer jdbc = new JDBCLayer();
         Application application = jdbc.lastApplication(Integer.parseInt(id)).get(0);
         return application;
     }
--- src/main/java/example/JDBCLayer.java
+++ src/main/java/example/JDBCLayer.java
@@ -1,7 +1,6 @@
 package example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -15,7 +14,13 @@ import java.util.List;
 @Repository
 public class JDBCLayer {
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    DataSource dataSource;
+
+    private JdbcTemplate jdbcTemplate;
+    @PostConstruct
+    public void init() {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     public List<Application> lastApplication(int contactId){
         final String QUERY_SQL =

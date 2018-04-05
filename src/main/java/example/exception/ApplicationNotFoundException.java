f01b50cb822baf434ceff81f803fbb0ade5cb340
igor <timohafclm@mail.ru>
2018-04-06 00:25:51 +0300

    extends choose XML or JSON

--- src/main/java/example/exception/ApplicationNotFoundException.java
+++ src/main/java/example/exception/ApplicationNotFoundException.java
@@ -1,4 +1,19 @@
 package example.exception;
 
-public class ApplicationNotFoundException extends Exception {
+public class ApplicationNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    private String errorMessage;
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public ApplicationNotFoundException(String errorMessage) {
+        super(errorMessage);
+        this.errorMessage = errorMessage;
+    }
+
+    public ApplicationNotFoundException() {
+        super();
+    }
 }
--- src/main/java/example/model/Application.java
+++ src/main/java/example/model/Application.java
@@ -1,25 +1,32 @@
 package example.model;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
+@XmlRootElement
 public class Application {
     private String APPLICATION_ID;
     private Integer CONTACT_ID;
     private Date DT_CREATED;
     private String PRODUCT_NAME;
 
+    @XmlElement
     public void setAPPLICATION_ID(String APPLICATION_ID) {
         this.APPLICATION_ID = APPLICATION_ID;
     }
 
+    @XmlElement
     public void setCONTACT_ID(Integer CONTACT_ID) {
         this.CONTACT_ID = CONTACT_ID;
     }
 
+    @XmlElement
     public void setDT_CREATED(Date DT_CREATED) {
         this.DT_CREATED = DT_CREATED;
     }
 
+    @XmlElement
     public void setPRODUCT_NAME(String PRODUCT_NAME) {
         this.PRODUCT_NAME = PRODUCT_NAME;
     }
